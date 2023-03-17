886c3a410dfc10eab4e9fbbd2af6731f699c4f07
lsa05 <110235270+lsa05@users.noreply.github.com>
2023-03-17 20:09:27 +0900

    [unrated] Title: 대충 만든 자판, Time: 2.47 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        
+        HashMap<Character, Integer> keyStore = new HashMap<>();
+        
+        for(String k : keymap){
+            for(int i=0; i<k.length(); i++){
+                
+                char c = k.charAt(i);
+                int click = keyStore.getOrDefault(c,i+1);
+                keyStore.put(c,Math.min(click,i+1));
+            }
+        }
+        int i = 0;
+        for(String t : targets){
+            
+            for(char c : t.toCharArray()){
+                if(keyStore.containsKey(c)){
+                    answer[i] += keyStore.get(c);
+                    
+                }else{
+                    answer[i] = -1;
+                    break;
+                }
+                
+            }i++;
+        }
+        
+        return answer;
+    }
+}
