9fda220db39516da8505b1cbe7aea7b7bf1dcff6
lsa05 <110235270+lsa05@users.noreply.github.com>
2023-03-17 14:27:29 +0900

    [unrated] Title: 둘만의 암호, Time: 0.99 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,22 +1,19 @@
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         
         for(char c : s.toCharArray()){
-            
             int idx = 0;
             
-            while(idx<index){
+            while(idx < index){
                 c = c=='z' ? 'a' : (char)(c+1);
                 
-                if(! skip.contains(String.valueOf(c))){
-                    idx ++;
+                if(!skip.contains(String.valueOf(c))){
+                    idx++;
                 }
-                
             }
-            answer += String.valueOf(c);
+            sb.append(String.valueOf(c));
         }
-        
-        return answer;
+        return sb.toString();
     }
 }
